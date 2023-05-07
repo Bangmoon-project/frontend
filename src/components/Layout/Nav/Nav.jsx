3c7c94376b62103cb6e7ac3b6d00c59ec9eb631b
v-va <poruch0915@gmail.com>
2023-05-07 23:49:55 +0900

    레이아웃 수정, Contents 디자인 Nav바 수정, 메인 페이지 디자인, 포스트페이지 생성, 라우터 수정

--- src/components/Layout/Nav/Nav.jsx
+++ src/components/Layout/Nav/Nav.jsx
@@ -8,28 +8,18 @@ const Nav = () => {
       <ul className={`${styles.menuContainer}`}>
 
         {/* ----- menu1Wrap ----- */}
-        <div className={`${styles.menuWrap1}`}>
           <li>HOME</li>
           <li>RANKING</li>
-        </div>
-
         {/* ----- menu2Wrap ----- */}
-        <div className={`${styles.menuWrap2}`}>
-
+        
           <li>
             <div className={`${styles.login}`}>
               <span className={`${styles.text}`}>LOGIN</span>
             </div>
           </li>
 
-          {/* <div className={`${styles.login}`}>
-              <span className={`${styles.text}`}>LOGIN</span>
-            </div> */}
-
-          <li>WRITE</li>
-        </div>
-
-
+          <li className={`${styles.write}`}>WRITE</li>
+          
       </ul>
     </nav>
   );
